3c271ee8a8da125336464b82b7afbf26adfaf1b4
Eric Luo <e2luo@uwaterloo.ca>
2019-07-14 02:05:46 -0400

    Splashscreen layout redone to scale on screens of all sizes.

--- JailbreakPong/app/src/main/java/com/kaze/jailbreakpong/SplashActivity.java
+++ JailbreakPong/app/src/main/java/com/kaze/jailbreakpong/SplashActivity.java
@@ -10,7 +10,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.spalsh_screen);
+        setContentView(R.layout.splash_screen);
 
         handler=new Handler();
         handler.postDelayed(new Runnable() {
