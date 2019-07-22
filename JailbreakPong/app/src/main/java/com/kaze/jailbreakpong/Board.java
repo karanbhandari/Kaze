d26aefe409a076071fd7858a62a96edee723dae0
Adil Mian <adilmian95@gmail.com>
2019-07-22 00:48:25 -0400

    Redid proper pausing/unpausing

--- JailbreakPong/app/src/main/java/com/kaze/jailbreakpong/Board.java
+++ JailbreakPong/app/src/main/java/com/kaze/jailbreakpong/Board.java
@@ -34,7 +34,7 @@ public class Board extends Observable {
 
     // during runtime, MainActivity tells us the screen dimensions in pixels, and the dpi
     public void init(Context context) {
-        state = State.PLAY;
+        state = State.BUILD;
         // square grid system that follows the 16:9 ratio
         numRows = 21; // default is 21 GridItems vertically across
         numCols = 12; // default is 12 GridItems horizontally across
